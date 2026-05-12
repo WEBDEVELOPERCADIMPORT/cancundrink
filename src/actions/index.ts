5feed3954f7eb34aed6011c589e4615b9b3b651f
KEVIN01306 <kevin60sanchez31@gmail.com>
2026-05-12 13:03:51 -0600

    ommmit - reviews

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,6 @@
 // src/actions/index.ts
-import { defineAction } from 'astro:actions';
+import { getReviews } from './reviews/getReviews.action';
 
 export const server = {
-    miAccion: defineAction({
-        handler: async (input) => {
-            return { success: true };
-        },
-    }),
+    getReviews
 };
